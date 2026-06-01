f1efa41aeeaa1a2acb21fdeb233e0f8ce40be7a9
izzytech-1 <israelikoko9@gmail.com>
2026-06-01 04:07:39 +0200

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -2,7 +2,7 @@ const controller = new AbortController();
 const timeout = setTimeout(() => controller.abort(), 25000);
 
 const response = await fetch(
-  'https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3',
+ 'https://api-inference.huggingface.co/models/HuggingFaceH4/zephyr-7b-beta',
   {
     method: 'POST',
     headers: {
